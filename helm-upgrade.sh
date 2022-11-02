e0048661f6794a12cc13469733956af12cdd1672
Raghav Agarwal <53235979+raghavag1997@users.noreply.github.com>
2022-11-02 12:27:01 +0530

    Update helm-upgrade.sh

--- helm-upgrade.sh
+++ helm-upgrade.sh
@@ -6,6 +6,8 @@ rev=$(helm list | awk '{ print $1 }' | grep hello-world)
 if [ "$rev" = "hello-world" ];
 then
         echo "Chart is already deployed upgrade the tag"
+        helm upgrade hello-world hello-world/ --set image.tag=$1
 else
         echo "Installing the chart"
+        helm install hello-world hello-world/
 fi;
